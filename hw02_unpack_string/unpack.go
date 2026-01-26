5fefa26e6b3d56355b54f0dcf7e72d955e7b757a
Natalia Kovaleva <delikanova.natalia@gmail.com>
2026-01-26 15:23:14 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	rs := []rune(s)
+	rsLen := len(rs)
+
+	if rsLen == 0 {
+		return "", nil
+	} else if err := validateString(rs); err != nil {
+		return "", err
+	}
+
+	var b strings.Builder
+	for i := 0; i < rsLen; i++ {
+		if i+1 < rsLen && unicode.IsDigit(rs[i+1]) {
+			count, err := strconv.Atoi(string(rs[i+1]))
+			if err != nil {
+				panic(err)
+			}
+			b.WriteString(strings.Repeat(string(rs[i]), count))
+
+			// digits should be skipped in result string
+			i++
+		} else {
+			b.WriteString(string(rs[i]))
+		}
+	}
+	return b.String(), nil
+}
+
+func validateString(rs []rune) error {
+	rsLen := len(rs)
+
+	// string should not begin with digit
+	if unicode.IsDigit(rs[0]) {
+		return ErrInvalidString
+	}
+
+	// string should not include numbers
+	for i := 1; i < rsLen; i++ {
+		if unicode.IsDigit(rs[i]) && unicode.IsDigit(rs[i-1]) {
+			return ErrInvalidString
+		}
+	}
+	return nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -17,7 +17,26 @@ func TestUnpack(t *testing.T) {
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
 		{input: "🙃0", expected: ""},
-		{input: "aaф0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "d\t3s2v0", expected: "d\t\t\tss"},
+		{input: "ab\"cd\"", expected: "ab\"cd\""},
+		{input: "ab\\cd\\", expected: "ab\\cd\\"},
+		{input: "\xff4\u00FF", expected: "����ÿ"},
+		{input: "日本語", expected: "日本語"},
+		{input: "\u65e52本\U00008a9e2", expected: "\u65e5\u65e5本\U00008a9e\U00008a9e"},
+		{input: "rr-1", expected: "rr-"},
+		{input: "%3@-", expected: "%%%@-"},
+		{input: "в4ф0", expected: "вввв"},
+		{input: "☀☁0☂3☃", expected: "☀☂☂☂☃"},
+		{input: "☄★💪", expected: "☄★💪"},
+		{input: "firs2t _1sec", expected: "firsst _sec"},
+		{input: " ", expected: " "},
+		{input: "f2 3s2", expected: "ff   ss"},
+		{input: "ⅰr", expected: "ⅰr"},
+		{input: "tⅦж", expected: "tⅦж"},
+		{input: "⅓e", expected: "⅓e"},
+		{input: "w⅓", expected: "w⅓"},
+		{input: "🔥2⓲", expected: "🔥🔥⓲"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -26,7 +45,6 @@ func TestUnpack(t *testing.T) {
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -36,9 +54,8 @@ func TestUnpack(t *testing.T) {
 }
 
 func TestUnpackInvalidString(t *testing.T) {
-	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	invalidStrings := []string{"3abc", "45", "aaa10b", "\u65e522本\U00008a9e2", "4🤘"}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
